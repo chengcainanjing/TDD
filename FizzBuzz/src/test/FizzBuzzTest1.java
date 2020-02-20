78014f93f1be6dfd9aaab7c628df3328585ed9e5
chengcainanjing <chengcainuist@gmail.com>
2020-02-20 21:38:27 +0800

    打卡第二天 fizzbuzz 练习

--- FizzBuzz/src/test/FizzBuzzTest1.java
+++ FizzBuzz/src/test/FizzBuzzTest1.java
@@ -1,11 +1,11 @@
 package test;
 
-import main.FizzBuzz;
 import main.FizzBuzz1;
-import org.junit.Assert.*;
+//import org.junit.Assert.*;
 import org.junit.Test;
-import org.junit.Assert;
+//import org.junit.Assert;
 
+import static main.FizzBuzz1.*;
 import static org.junit.Assert.*;
 
 
@@ -13,10 +13,10 @@ public class FizzBuzzTest1 {
     @Test
     public void TestPrint() {
         FizzBuzz1 fizz1 = new FizzBuzz1();
-        assertEquals("1", FizzBuzz1.pass(1));
-        assertEquals("Fizz", FizzBuzz1.pass(3));
-        assertEquals("Buzz", FizzBuzz1.pass(5));
-        assertEquals("FizzBuzz", FizzBuzz1.pass(15));
+        assertEquals("1", pass(1));
+        assertEquals("Fizz", pass(3));
+        assertEquals("Buzz", pass(5));
+        assertEquals("FizzBuzz", pass(15));
         fizz1.printNum();
     }
 }
