f22907419b9e5ccec145d40481c745338d4af58b
chengcainanjing <chengcainuist@gmail.com>
2020-02-22 20:09:26 +0800

    打卡第三天 fizzbuzz 练习

--- FizzBuzz/src/main/FizzBuzz2.java
+++ FizzBuzz/src/main/FizzBuzz2.java
@@ -1,5 +1,20 @@
 package main;
 
+/**
+ * This program demonstrates FizzBuzz2.
+ * 程序功能：
+ * 1.打印出从1到100的数字
+ *
+ * 2.将其中3的倍数替换成“Fizz”
+ *
+ * 3.5的倍数替换成“Buzz”
+ *
+ * 4.既能被3整除、又能被5整除的数则替换成“FizzBuzz”
+ *
+ * @author chengcai
+ * @version 1.0 2020.02.22
+ */
+
 public class FizzBuzz2 {
     public static String pass(int Num) {
         if ((Num % 15) == 0) return "FizzBuzz";
--- FizzBuzz/src/test/FizzBuzzTest2.java
+++ FizzBuzz/src/test/FizzBuzzTest2.java
@@ -4,6 +4,22 @@ import main.FizzBuzz2;
 import org.junit.Assert;
 import org.junit.Test;
 
+
+/**
+ * This program demonstrates FizzBuzzTest2.
+ * 程序功能：
+ * 1.打印出从1到100的数字
+ *
+ * 2.将其中3的倍数替换成“Fizz”
+ *
+ * 3.5的倍数替换成“Buzz”
+ *
+ * 4.既能被3整除、又能被5整除的数则替换成“FizzBuzz”
+ *
+ * @author chengcai
+ * @version 1.0 2020.02.22
+ */
+
 public class FizzBuzzTest2 {
     @Test
     public void printNum() {
