4cb7417dc9d4d7e93ae59fcde8c705daf8fe71ac
chengcainanjing <chengcainuist@gmail.com>
2020-03-04 16:16:38 +0800

    Revert "打卡第四天 Args 练习"
    This reverts commit 90389dea

---
+++ Sources/src/main/Args2.java
@@ -0,0 +1,97 @@
+package main;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author chengcai
+ * @version 2.0 2020.03.02
+ */
+
+
+public class Args2 {
+    public static String strArrayCmd = "";
+    Map<String, String> values = new HashMap<>();
+
+    public Args2(String strCmd) {
+        String name;
+        String value;
+        if (getCmdFormat(strCmd)) {
+            String[] arrayCmd = strCmd.split("\\s+");
+            for (int i = 0; i <= arrayCmd.length - 1; i++) {
+                if (arrayCmd[i].contains("-")) {
+                    if (i + 1 < arrayCmd.length) {
+                        System.out.println(arrayCmd[i + 1].charAt(1));
+                        if (arrayCmd[i + 1].charAt(0) != '-' || arrayCmd[i + 1].length() > 2) {
+                            name = arrayCmd[i];
+                            value = arrayCmd[i + 1];
+                            values.put(name, value);
+                            i++;
+                        }
+                        else if (Character.getNumericValue(arrayCmd[i + 1].charAt(1)) <= 9 && Character.getNumericValue(arrayCmd[i + 1].charAt(1)) >= 0 ) {
+                            name = arrayCmd[i];
+                            value = arrayCmd[i + 1];
+                            values.put(name, value);
+                            i++;
+                        } else {
+                            name = arrayCmd[i];
+                            value = null;
+                            values.put(name, value);
+                        }
+                    } else {
+                        name = arrayCmd[i];
+                        value = null;
+                        values.put(name, value);
+                    }
+                }
+            }
+        }
+    }
+
+    private boolean isValue(String value) {
+        if (value.charAt(0) == '-') {
+            if (value.length() > 2) {
+                return true;
+            }
+            if (value.charAt(1) >= '0' && value.charAt(1) <= '9') {
+                return true;
+            }
+            return false;
+        }
+        return true;
+    }
+
+    private Object getType(String name, String strValue) {
+        switch (name) {
+            case "-l":
+                if (strValue == null || strValue == "false") {
+                    return Boolean.FALSE;
+                } else return Boolean.TRUE;
+            case "-d":
+                return strValue;
+            case "-p":
+                if (strValue == null) {
+                    return Integer.valueOf(0);
+                } else return Integer.valueOf(strValue);
+            default:
+                return strValue;
+
+        }
+    }
+
+    private String getsStrValue(String name) {
+        return values.get(name);
+    }
+
+    public Boolean getCmdFormat(String strCmd) {
+        if (strCmd.charAt(0) != '-') {
+            return false;
+        } else return true;
+
+    }
+
+    public Object getValues(String name) {
+        return getType(name, getsStrValue(name));
+    }
+}
+
---
+++ Sources/src/test/ArgsTest2.java
@@ -0,0 +1,55 @@
+import main.Args1;
+import org.junit.Assert;
+import org.junit.Test;
+
+
+/**
+ * @author chengcai
+ * @version 2.0 2020.03.02
+ */
+
+public class ArgsTest2 {
+    @Test
+    public void testNormalCmd() {
+        Args1 args=new Args1("-l true -d /usr/local -p 8080");
+        assertEquals(true,args.getValues("-l") );
+        assertEquals("/usr/local",args.getValues("-d") );
+        assertEquals(8080,args.getValues("-p") );
+    }
+
+    @Test
+    public void testCmdNegetive() {
+        Args1 args=new Args1("-l -p -d /usr/local");
+        assertEquals(false,args.getValues("-l") );
+        assertEquals("/usr/local",args.getValues("-d") );
+        assertEquals(0,args.getValues("-p") );
+    }
+
+    @Test
+    public void testCmdMoreParameter() {
+        Args1 args=new Args1("-l -p -9 -d /usr/local -g this,is,a,list");
+        Assert.assertEquals(false, args.getValues("-l"));
+        assertEquals("/usr/local", args.getValues("-d"));
+        assertEquals(-9, args.getValues("-p"));
+        assertEquals("this,is,a,list",args.getValues("-g"));
+    }
+    @Test
+    public void testCmdFormat(){
+        Args1 args=new Args1("l -p -9 -d /usr/local");
+        assertEquals(false,args.getCmdFormat("l -p -9 -d /usr/local"));
+        assertEquals(true,args.getCmdFormat("-l -p -9 -d /usr/local -g this,is,a,list"));
+    }
+
+    private void  assertEquals(String Str, Object Name) {
+        Assert.assertEquals(Str,Name);
+    }
+    private void  assertEquals(int Num, Object Name) {
+        Assert.assertEquals(Num,Name);
+    }
+    private void  assertEquals(boolean Bool, Object name) {
+        Assert.assertEquals(Bool, name);
+    }
+
+
+}
+
